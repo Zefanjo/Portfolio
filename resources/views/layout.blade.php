25e3f86cbddc122b82de31432004f1501fb4b2c3
Zefanjo <146831639+Zefanjo@users.noreply.github.com>
2024-02-09 13:11:07 +0100

    fixed characters

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Zéfanjo Mulder</title>
-    @vite(['resources/css/app.css', 'resources/js/script.js'])w8w
+    @vite(['resources/css/app.css', 'resources/js/script.js'])
 </head>
 <body>
 <div id="englishText">
